805d0297998b044efb2ee3cbc452a6d3984eb09d
Cai Martin <cmartin.professional@yahoo.com>
2019-06-21 13:05:39 -0400

    Made action and reducer

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,7 +1,29 @@
-/* 
+/*
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+const axios = require("axios");
+//GET
+export const GETSMURF = "FETCHINGSMURF";
+export const GETSUCCESS = "GETSUCCESS";
+export const GETFAILURE = "GETFAILURE";
+//POST
+export const ADDSMURF = "ADDSMURF";
+export const ADDSUCCESS = "ADDSUCCESS";
+export const ADDFAILURE = "ADDFAILURE";
+//PUT
+export const EDITSMURF = "EDITSMURF";
+export const EDITSUCCESS = "EDITSUCCESS";
+export const EDITFAILURE = "EDITFAILURE";
+//DELETE
+export const REMOVEFETCH = "REMOVEFETCH";
+export const REMOVESUCCESS = "REMOVESUCCESS";
+export const REMOVEFAILURE = "REMOVEFAILURE";
+
+//LOGIN
+export const LOGINFETCH = "LOGINFETCH";
+export const LOGINSUCCESS = "LOGINSUCCESS";
+export const LOGINFAILURE = "LOGINFAILURE";
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
@@ -13,3 +35,91 @@
    U - updateSmurf
    D - deleteSmurf
 */
+export const makeSmurf = user => dispatch => {
+  dispatch({ type: FETCHINGSmurf });
+  axios
+    .get("/Smurf", {
+      headers: {
+        authorization: getUser().token
+      }
+    })
+    .then(res => {
+      dispatch({ type: GETSUCCESS, payload: res.data });
+    })
+    .catch(res => {
+      dispatch({
+        type: GETFAILURE,
+        payload: res.data
+      });
+    });
+};
+export const addSmurf = FRIEND => dispatch => {
+  dispatch({ type: ADDSmurf });
+  axios
+    .post(
+      "/smurf",
+      {
+        name: FRIEND.name,
+        age: FRIEND.age,
+        email: FRIEND.email
+      },
+      {
+        headers: {
+          authorization: getUser().token
+        }
+      }
+    )
+    .then(res => {
+      dispatch({ type: ADDSUCCESS, payload: res.data });
+    })
+    .catch(res => {
+      dispatch({
+        type: ADDFAILURE,
+        payload: res.data
+      });
+    });
+};
+export const editSmurf = (FRIEND, id) => dispatch => {
+  dispatch({ type: EDITSmurf });
+  axios
+    .put(
+      `/smurf/${FRIEND.id}`,
+      {
+        name: FRIEND.name,
+        age: FRIEND.age,
+        email: FRIEND.email
+      },
+      {
+        headers: {
+          authorization: getUser().token
+        }
+      }
+    )
+    .then(res => {
+      dispatch({ type: EDITSUCCESS, payload: res.data });
+    })
+    .catch(res => {
+      dispatch({
+        type: EDITFAILURE,
+        payload: res.data
+      });
+    });
+};
+export const removeSmurf = (id, user) => dispatch => {
+  dispatch({ type: REMOVEFETCH });
+  axios
+    .delete(`/smurf/${id}`, {
+      headers: {
+        authorization: getUser().token
+      }
+    })
+    .then(res => {
+      dispatch({ type: REMOVESUCCESS, payload: res.data });
+    })
+    .catch(res => {
+      dispatch({
+        type: REMOVEFAILURE,
+        payload: res.data
+      });
+    });
+};
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,6 +1,20 @@
 /*
   Be sure to import in all of the action types from `../actions`
 */
+import {
+  GETSMURF,
+  GETSUCCESS,
+  GETFAILURE,
+  ADDSMURF,
+  ADDSUCCESS,
+  ADDFAILURE,
+  EDITSMURF,
+  EDITSUCCESS,
+  EDITFAILURE,
+  REMOVEFETCH,
+  REMOVESUCCESS,
+  REMOVEFAILURE
+} from "../actions";
 
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
@@ -16,8 +30,120 @@
 
 /*
   You'll only need one smurf reducer for this project.
-  Feel free to export it as a default and import as rootReducer. 
+  Feel free to export it as a default and import as rootReducer.
   This will guard your namespacing issues.
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+
+const initialState = {
+  SMURF: [],
+  isloadingGET: false,
+  successGET: false,
+  isloadingPOST: false,
+  successPOST: false,
+  isloadingPUT: false,
+  successPUT: false,
+  isloadingDELETE: false,
+  successDELTE: false,
+  isloadingLOGIN: false,
+  successLOGIN: false,
+  user: ""
+};
+export default (state = initialState, action) => {
+  switch (action.type) {
+    //GET
+    case FETCHINGSMURF:
+      return {
+        ...state,
+        SMURF: [],
+        isloadingGET: true
+      };
+    case GETSUCCESS:
+      return {
+        ...state,
+        SMURF: action.payload,
+        isloadingGET: false,
+        successGET: true
+      };
+    case GETFAILURE:
+      return {
+        ...state,
+        SMURF: [],
+        isloadingGET: false,
+        successGET: false
+      };
+    //POST
+    case ADDSMURF:
+      return {
+        ...state,
+        isloadingPOST: false,
+        successPOST: true
+      };
+    case ADDSUCCESS:
+      return {
+        ...state,
+        SMURF: [...state.SMURF, action.payload],
+        isloadingPOST: false,
+        successPOST: true
+      };
+    case ADDFAILURE:
+      return {
+        ...state,
+        isloadingPOST: false,
+        successPOST: false
+      };
+    //PUT
+    case EDITSMURF:
+      return {
+        ...state,
+        isloadingPUT: true,
+        successPUT: false
+      };
+
+    case EDITSUCCESS:
+      return {
+        ...state,
+        SMURF: state.SMURF.map(smurf => {
+          if (smurf.id !== action.payload) {
+            return smurf;
+          }
+        }),
+        isloadingPUT: false,
+        successPUT: true
+      };
+    case EDITFAILURE:
+      return {
+        ...state,
+        isloadingPUT: false,
+        successPUT: false
+      };
+    //DELETE
+    case REMOVEFETCH:
+      return {
+        ...state,
+        isloadingDELETE: true,
+        successDELETE: false
+      };
+    case REMOVESUCCESS:
+      return {
+        ...state,
+        SMURF: state.SMURF.map(friend => {
+          if (smurf.id !== action.payload) {
+            return smurf;
+          }
+        }),
+        isloadingDELETE: false,
+        successDELETE: true
+      };
+    case REMOVEFAILURE:
+      return {
+        ...state,
+        isloadingDELETE: false,
+        successDELETE: false
+      };
+
+    default:
+      return state;
+  }
+};
